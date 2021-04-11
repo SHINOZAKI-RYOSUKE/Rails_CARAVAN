80ee66473572a357a0d37d6e42bdef495d57350e
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-04-11 11:24:30 +0000

    [update4]

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  
+
   def index
     @blogs = Blog.all
   end
@@ -24,9 +24,20 @@ class BlogsController < ApplicationController
 
 
   def edit
+    @blog = Blog.find(params[:id])
   end
 
-
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
 
   private
   def blog_params
